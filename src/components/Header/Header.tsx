0e6ecc730387fc4cb80079189aad110f081e7c8a
Antoine Ando <antoine.ando@gmail.com>
2024-10-07 13:11:14 +0200

    Theme toggle

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import style from "./Header.module.scss";
+import ThemeToggle from "../ThemeToggle/ThemeToggle";
 
 type Props = {};
 
 function Header({}: Props) {
   return (
     <header className={style.header}>
-      <div className={style.spacer}></div>
+      <div className={style.themeToggleSlot}>
+        <ThemeToggle />
+      </div>
       <h1 className={style.title}>Weather App</h1>
       <button className={style.searchButton}>🔍</button>
     </header>
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+import ThemeProvider from "./providers/ThemeProvider.tsx";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </StrictMode>
+);
