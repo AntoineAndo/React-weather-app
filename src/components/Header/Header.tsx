4c88c549f7f2b14b133c46bf35861958b55c27ca
Antoine Ando <antoine.ando@gmail.com>
2024-10-07 12:43:06 +0200

    Initial commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.scss";
+
+type Props = {};
+
+function Header({}: Props) {
+  return (
+    <header>
+      <p>San Francisco</p>
+    </header>
+  );
+}
+
+export default Header;
