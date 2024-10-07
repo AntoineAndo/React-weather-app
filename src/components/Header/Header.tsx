1b7d2a201cdf723231d21675e9cdbfaf5c068404
Antoine Ando <antoine.ando@gmail.com>
2024-10-07 12:57:39 +0200

    Basic header layout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-import "./Header.scss";
+import style from "./Header.module.scss";
 
 type Props = {};
 
 function Header({}: Props) {
   return (
-    <header>
-      <p>San Francisco</p>
+    <header className={style.header}>
+      <div className={style.spacer}></div>
+      <h1 className={style.title}>Weather App</h1>
+      <button className={style.searchButton}>🔍</button>
     </header>
   );
 }
